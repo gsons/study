7a785058c1551c8a286e08f67c7461725219e8d2
gsons <976955017@qq.com>
2023-08-14 17:03:59 +0800

    quick

--- cpp/sort/quick_order.c
+++ cpp/sort/quick_order.c
@@ -1,7 +1,35 @@
-#include <stdlib.h> // 包含标准库以使用 malloc 和 free 函数
 #include <stdio.h>
 
+int partition(int data[], int low, int high)
+{
+    int i, j, pivot;
+    pivot = data[low]; // 选择第一个元素作为基准值
+    i = low;
+    j = high;
 
+    while (i < j)
+    {
+        // 从数组的两端交替地向中间扫描 从左边找到一个不符合的元素，再从右边找到一个不符合的元素，彼此之间相互交换位置
+        while (i < j && data[j] >= pivot)
+            j--;
+        data[i] = data[j]; // 将小于基准值的元素移到左侧
+        while (i < j && data[i] <= pivot)
+            i++;
+        data[j] = data[i]; // 将大于基准值的元素移到右侧
+    }
+    data[i] = pivot; // 放置基准值到正确位置
+    return i;        // 返回基准值的位置
+}
+
+void quickSort(int data[], int low, int high)
+{
+    if (low < high)
+    {
+        int loc = partition(data, low, high); // 获取基准值位置
+        quickSort(data, low, loc - 1);        // 对左侧子数组递归排序
+        quickSort(data, loc + 1, high);       // 对右侧子数组递归排序
+    }
+}
 
 int main()
 {
@@ -14,7 +42,7 @@ int main()
         printf("%d ", data[i]);
     }
 
-    select_sort(data, n);
+    quickSort(data, 0, n - 1);
 
     printf("\n排序后数组：\n");
     for (int i = 0; i < n; i++)
